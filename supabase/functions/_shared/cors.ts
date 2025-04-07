b227be4ad1a414fe3e29ce53d9c43b5b9a90cc18
swbam <2728451+swbam@users.noreply.github.com>
2025-04-06 20:26:13 -0500

    edit

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*', // Allow all origins for now, restrict in production
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-application-name',
   'Access-Control-Allow-Methods': 'POST, OPTIONS', // Allow POST and OPTIONS
 };
